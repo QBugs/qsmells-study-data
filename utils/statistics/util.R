1c0875d53edf53b84c86a22bbabea6d864959e99
jose <jcmcampos@fc.ul.pt>
2021-12-10 17:01:05 +0000

    Added a useful set of R functions.

---
+++ utils/statistics/util.R
@@ -0,0 +1,47 @@
+# ------------------------------------------------------------------------------
+# A set of utility functions.
+# ------------------------------------------------------------------------------
+
+# Common external packages
+library('data.table') # install.packages('data.table')
+library('stringr') # install.packages('stringr')
+library('ggplot2') # install.packages('ggplot2')
+
+# --------------------------------------------------------------------- Wrappers
+
+'%!in%' <- function(x,y)!('%in%'(x,y)) # Wrapper to 'not in'
+
+load_CSV <- function(csv_path) {
+  return (read.csv(csv_path, header=TRUE, stringsAsFactors=FALSE))
+}
+
+load_CSV_GZ <- function(csv_path) {
+  return (read.table(gzfile(csv_path), header=TRUE, stringsAsFactors=FALSE))
+}
+
+replace_string <- function(string, find, replace) {
+  gsub(find, replace, string)
+}
+
+embed_fonts_in_a_pdf <- function(pdf_path) {
+  library('extrafont') # install.packages('extrafont')
+  embed_fonts(pdf_path, options='-dSubsetFonts=true -dEmbedAllFonts=true -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dMaxSubsetPct=100')
+}
+
+# ------------------------------------------------------------------------- Plot
+
+#
+# Plots the provided text on a dedicated page.  This function is usually used to
+# separate plots for multiple analyses in the same PDF.
+#
+plot_label <- function(text) {
+  library('ggplot2') # install.packages('ggplot2')
+  p <- ggplot() + annotate('text', label=text, x=4, y=25, size=8) + theme_void()
+  print(p)
+}
+
+# ---------------------------------------------------------------- Study related
+
+# TODO
+
+# EOF
