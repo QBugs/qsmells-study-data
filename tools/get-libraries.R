42145315af0b84e8cb1a0388ac182fc7fcda8f7e
jose <jcmc@fe.up.pt>
2022-09-02 11:29:24 +0100

    Updated set of required R packages

--- tools/get-libraries.R
+++ tools/get-libraries.R
@@ -1,18 +1,16 @@
 # R repository
 repository="http://cran.us.r-project.org"
 # Install packages
-install.packages('this.path', repos=repository)
 install.packages('data.table', repos=repository)
 install.packages('stringr', repos=repository)
 install.packages('ggplot2', repos=repository)
-install.packages('extrafont', repos=repository)
-install.packages('testthat', repos=repository)
+install.packages('irr', repos=repository)
+install.packages('caret', repos=repository)
 # Load libraries (aka runtime sanity check)
-library('this.path')
 library('data.table')
 library('stringr')
 library('ggplot2')
-library('extrafont')
-library('testthat')
+library('irr')
+library('caret')
 # Exit
 quit(save="no", status=0)
