b7603c3fc069d6e6cdc0d17a480789baa113a136
jose <jcmc@fe.up.pt>
2023-02-09 15:17:34 +0000

    No need to seet locale.

--- utils/scripts/utils.sh
+++ utils/scripts/utils.sh
@@ -3,10 +3,6 @@
 UTILS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 USER_HOME_DIR="$(cd ~ && pwd)"
 
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
-
 #
 # Print error message to the stdout and exit.
 #
