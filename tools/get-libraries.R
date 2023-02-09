8808919cd65db11f5490c7d1b888df2751d99c9f
jose <jcmc@fe.up.pt>
2023-02-09 12:39:07 +0000

    Updated set of deps.

--- tools/get-libraries.R
+++ tools/get-libraries.R
@@ -9,20 +9,24 @@ PATH <- args[1]
 source(paste(PATH, '/../utils/statistics/utils.R', sep=''))
 # R repository
 repository <- 'https://cloud.r-project.org'
-# Install packages
+
+# Install and load packages (aka runtime sanity check)
 install.packages('data.table', lib=local_library, repos=repository)
-install.packages('stringr', lib=local_library, repos=repository)
-install.packages('lpSolve', lib=local_library, repos=repository) # required by irr
-install.packages('irr', lib=local_library, repos=repository)
-install.packages('ggplot2', lib=local_library, repos=repository) # required by caret
-install.packages('caret', lib=local_library, repos=repository)
-# Load libraries (aka runtime sanity check)
 library('data.table', lib.loc=local_library)
+install.packages('stringr', lib=local_library, repos=repository)
 library('stringr', lib.loc=local_library)
+install.packages('lpSolve', lib=local_library, repos=repository) # required by irr
 library('lpSolve', lib.loc=local_library)
+install.packages('irr', lib=local_library, repos=repository)
 library('irr', lib.loc=local_library)
+install.packages('withr', lib=local_library, repos=repository) # required by ggplot2
+library('withr', lib.loc=local_library)
+install.packages('ggplot2', lib=local_library, repos=repository) # required by caret
 library('ggplot2', lib.loc=local_library)
+install.packages('caret', lib=local_library, repos=repository)
 library('caret', lib.loc=local_library)
+
 # Exit
 quit(save='no', status=0)
+
 # EOF
