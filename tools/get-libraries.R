3f167dde138c54d3bb119cc407880bd1e1b53f0f
jose <jcmc@fe.up.pt>
2023-02-09 09:49:51 +0000

    Added missing deps.

--- tools/get-libraries.R
+++ tools/get-libraries.R
@@ -12,12 +12,16 @@ repository <- 'https://cloud.r-project.org'
 # Install packages
 install.packages('data.table', lib=local_library, repos=repository)
 install.packages('stringr', lib=local_library, repos=repository)
+install.packages('lpSolve', lib=local_library, repos=repository) # required by irr
 install.packages('irr', lib=local_library, repos=repository)
+install.packages('ggplot2', lib=local_library, repos=repository) # required by caret
 install.packages('caret', lib=local_library, repos=repository)
 # Load libraries (aka runtime sanity check)
 library('data.table', lib.loc=local_library)
 library('stringr', lib.loc=local_library)
+library('lpSolve', lib.loc=local_library)
 library('irr', lib.loc=local_library)
+library('ggplot2', lib.loc=local_library)
 library('caret', lib.loc=local_library)
 # Exit
 quit(save='no', status=0)
